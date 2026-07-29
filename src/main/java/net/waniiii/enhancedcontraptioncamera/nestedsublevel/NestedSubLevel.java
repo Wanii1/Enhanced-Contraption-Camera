1eadb3d9a224791f903963b06604249ad9eaf72a
Wanii1 <145401639+Wanii1@users.noreply.github.com>
2026-07-29 17:13:28 -0300

    Multy ty

---
+++ src/main/java/net/waniiii/enhancedcontraptioncamera/nestedsublevel/NestedSubLevel.java
@@ -0,0 +1,14 @@
+package net.waniiii.enhancedcontraptioncamera.nestedsublevel;
+
+
+import dev.ryanhcode.sable.sublevel.SubLevel;
+import dev.simulated_team.simulated.content.blocks.swivel_bearing.link_block.SwivelBearingPlateBlockEntity;
+
+import java.util.List;
+
+
+public class NestedSubLevel {
+    public static List<SubLevel> SubGroup(){
+        List<SubLevel> = new List<SubLevel.>()
+    }
+}
